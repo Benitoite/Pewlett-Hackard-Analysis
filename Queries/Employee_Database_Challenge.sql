ed70c6e3f2b7c53fe33034e8f258fa450deaa707
Richard Barber <kd6kxr@gmail.com>
2021-09-27 20:25:22 -0700

    Add deliverable 2 files

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,131 @@
+-- Module Challenge Deliverable 1
+
+--Join employee info with title info, filtered by a date range
+SELECT e.emp_no, e.first_name, e.last_name,
+    ttl.title, ttl.from_date, ttl.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ttl
+ON e.emp_no = ttl.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+    first_name,
+    last_name,
+    title
+INTO distinct_retires
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+--Get counts per title
+SELECT count(title), title
+INTO retire_title_counts
+FROM distinct_retires
+GROUP BY title
+ORDER BY count DESC;
+
+--Module Challenge Deliverable 2
+
+--Join employee info with title info, filtered by a date range
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+    de.from_date, de.to_date,
+	ttl.title
+INTO mentor_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as ttl
+ON e.emp_no = ttl.emp_no
+WHERE de.to_date = '9999-01-01'
+	   AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+
+
+
+
+--Module lesson code:
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_counts
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no,
+    first_name,
+    last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');	
+
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	      AND (de.to_date = '9999-01-01');
+		  
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+SELECT ce.emp_no,
+    ce.first_name,
+    ce.last_name,
+    d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--Skill Drill
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    d.dept_name
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE dept_name IN ('Sales', 'Development')
+ORDER BY dept_name;
+
+
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -100,123 +228,3 @@ WHERE de.to_date = ('9999-01-01');
 
 
 
--- Module Challenge
-SELECT e.emp_no, e.first_name, e.last_name,
-    ttl.title, ttl.from_date, ttl.to_date
-INTO retirement_titles
-FROM employees as e
-LEFT JOIN titles as ttl
-ON e.emp_no = ttl.emp_no
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY emp_no;
-
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
-    first_name,
-    last_name,
-    title
-INTO distinct_retires
-FROM retirement_titles
-ORDER BY emp_no, to_date DESC;
-
-
-
-SELECT count(title), title
-INTO retire_title_counts
-FROM distinct_retires
-GROUP BY title
-ORDER BY count DESC;
-
-
-
-
-SELECT * FROM current_emp;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Employee count by department number
-SELECT COUNT(ce.emp_no), de.dept_no
-INTO emp_counts
-FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no
-ORDER BY de.dept_no;
-
-SELECT * FROM salaries
-ORDER BY to_date DESC;
-
-SELECT emp_no,
-    first_name,
-last_name,
-    gender
-INTO emp_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');	
-
-SELECT e.emp_no,
-    e.first_name,
-e.last_name,
-    e.gender,
-    s.salary,
-    de.to_date
-INTO emp_info
-FROM employees as e
-INNER JOIN salaries as s
-ON (e.emp_no = s.emp_no)
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
-	      AND (de.to_date = '9999-01-01');
-		  
--- List of managers per department
-SELECT  dm.dept_no,
-        d.dept_name,
-        dm.emp_no,
-        ce.last_name,
-        ce.first_name,
-        dm.from_date,
-        dm.to_date
-INTO manager_info
-FROM dept_manager AS dm
-    INNER JOIN departments AS d
-        ON (dm.dept_no = d.dept_no)
-    INNER JOIN current_emp AS ce
-        ON (dm.emp_no = ce.emp_no);
-		
-SELECT ce.emp_no,
-    ce.first_name,
-    ce.last_name,
-    d.dept_name
--- INTO dept_info
-FROM current_emp as ce
-INNER JOIN dept_emp AS de
-ON (ce.emp_no = de.emp_no)
-INNER JOIN departments AS d
-ON (de.dept_no = d.dept_no);
-
---Skill Drill
-SELECT e.emp_no,
-    e.first_name,
-    e.last_name,
-    d.dept_name
-FROM employees as e
-INNER JOIN dept_emp AS de
-ON (e.emp_no = de.emp_no)
-INNER JOIN departments AS d
-ON (de.dept_no = d.dept_no)
-WHERE dept_name IN ('Sales', 'Development')
-ORDER BY dept_name;
