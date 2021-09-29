fa804cca787785390f45edebb1b523f9d86d5a2d
Benitoite <kd6kxr@gmail.com>
2021-09-28 18:10:33 -0700

    Update Employee_Database_Challenge.sql

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,106 @@
+-- Initialize all the Tables.
+-- Creating tables...
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+CREATE TABLE employees (
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+	from_date DATE,
+	to_date DATE,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+);
+
+SELECT * FROM retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Perform some preliminary analyses.
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- Specify Column Data
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+-- Create New Table To Hold Data
+INTO current_emp
+-- Join Two Tables
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+-- Filter to currently employed
+WHERE de.to_date = ('9999-01-01');
+
+
+
+-- Module Challenge
+
 -- Module Challenge Deliverable 1
 
 --Join employee info with title info, filtered by a date range
@@ -113,8 +216,11 @@ ORDER BY de.dept_no;
 	  
 SELECT * FROM mentors_3_expanded;
 
---Module lesson code:
 
+
+
+
+--Module lesson practice code:
 
 -- Employee count by department number
 SELECT COUNT(ce.emp_no), de.dept_no
@@ -191,107 +297,5 @@ INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no)
 WHERE dept_name IN ('Sales', 'Development')
 ORDER BY dept_name;
-
-
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
-);
-
-CREATE TABLE employees (
-	 emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_emp (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR NOT NULL,
-	from_date DATE,
-	to_date DATE,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-);
-
-CREATE TABLE titles (
-    emp_no INT NOT NULL,
-    title VARCHAR NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-);
-
-SELECT * FROM retirement_info;
-
--- Create new table for retiring employees
-SELECT emp_no, first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- Check the table
-SELECT * FROM retirement_info;
-
-
-
-
--- Joining departments and dept_manager tables
-SELECT d.dept_name,
-     dm.emp_no,
-     dm.from_date,
-     dm.to_date
-FROM departments as d
-INNER JOIN dept_manager as dm
-ON d.dept_no = dm.dept_no;
-
--- Joining retirement_info and dept_emp tables
-SELECT ri.emp_no,
-    ri.first_name,
-ri.last_name,
-    de.to_date
-FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no;
-
--- Specify Column Data
-SELECT ri.emp_no,
-    ri.first_name,
-    ri.last_name,
-    de.to_date
--- Create New Table To Hold Data
-INTO current_emp
--- Join Two Tables
-FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no
--- Filter to currently employed
-WHERE de.to_date = ('9999-01-01');
-
 
 
